47d83f990b5ddbae1b0a765399a5206d3f3a590f
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-07-05 20:35:54 +0530

    Setting Up User View Model

--- app/src/main/java/wary/bhaskar/learningroomdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/wary/bhaskar/learningroomdatabase/fragments/list/ListFragment.kt
@@ -5,13 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_list.view.*
 import wary.bhaskar.learningroomdatabase.R
+import wary.bhaskar.learningroomdatabase.data.UserViewModel
 
 class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // Recyclerview
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
     }
+
 }
